e5ffcb78f395d022afbb1ee8128835860c1a2ff5
BondedIncome <daniotmw10@gmail.com>
2025-06-20 14:13:27 +0300

    Budget App Test

---
+++ ScientificComputingWithPythonProjects/Build a Budget App Project/test.py
@@ -0,0 +1,11 @@
+from budgetApp import Category, create_spend_chart
+
+food = Category('Food')
+food.deposit(1000, 'deposit')
+food.withdraw(10.15, 'groceries')
+food.withdraw(15.89, 'restaurant and more food for dessert')
+clothing = Category('Clothing')
+food.transfer(50, clothing)
+print(food)
+
+print(create_spend_chart([food,clothing,]))
