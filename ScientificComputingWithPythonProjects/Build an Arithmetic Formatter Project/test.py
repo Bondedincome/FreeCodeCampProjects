88c960cd67cad74f64015c472a7e0f57ac1ef1ba
BondedIncome <daniotmw10@gmail.com>
2025-06-20 15:56:41 +0300

    Arithmetic Formatter Test

---
+++ ScientificComputingWithPythonProjects/Build an Arithmetic Formatter Project/test.py
@@ -0,0 +1,4 @@
+from arthiFormatter import arithmetic_arranger
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
